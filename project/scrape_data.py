8b8c1ecaa8056c78c90c017ff193a411f0cab571
LuminousCodeGH <jacob@reaves.org>
2022-11-22 00:23:15 +0100

    Added searching for courses using iteration

--- project/scrape_data.py
+++ project/scrape_data.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from data.creds import net_id, password, COURSE_URL, COURSE_SEARCH
+from selenium.webdriver.remote.webelement import WebElement
+from data.creds import net_id, password, COURSE_BOX, COURSE_MSG, COURSE_BTN, SIGN_UP_URL, HOME_URL
 import logging
 
 
@@ -25,7 +26,8 @@ class Scraper:
         if (not courses):
             logging.warning("The courses dict is empty")
         self._courses = courses
-        self._incomplete_courses = {course:False for (course, complete) in courses if not complete}
+        self._incomplete_courses = {course:False for (course, complete) in courses.items() if not complete}
+        # TODO: Registrate builds incomplete courses based off of the grades
 
     @property
     def driver(self) -> Chrome | Firefox:
@@ -39,21 +41,46 @@ class Scraper:
 
     def scrape_for_courses(self):
         d: Chrome | Firefox = self.driver
-        d.get(COURSE_URL)
+        d.get("https://my.tudelft.nl/#/inschrijven/cursus/:id")
         self._wait_for_element_by(By.XPATH, '//*[@id="submit_button"]')
 
         d.find_element(By.XPATH, '//*[@id="username"]').send_keys(net_id)
         d.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
         d.find_element(By.XPATH, '//*[@id="submit_button"]').click()
 
-        self._wait_for_element_by(By.XPATH, COURSE_SEARCH)
+        for course in self._incomplete_courses:
+            self._wait_for_element_by(By.CLASS_NAME, "searchbar-input")
+            d.find_element(By.CLASS_NAME, "searchbar-input").send_keys(course)
+            self._wait_for_element_by(By.XPATH, COURSE_BOX)
+            d.find_element(By.XPATH, COURSE_BOX).click()
+            
+            i: int = 0
+            while (True or i < 1000):
+                i += 1
+                if (self._search_in_page("Helaas")):
+                    logging.info(f"You are not able to sign up for '{course}'")
+                    break
+                elif (self._search_in_page("Selecteer een toetsgelegenheid")):
+                    logging.warning(f"Not implemented!")
+                    break
+                    # TODO: Implement if course is available
+            self._refresh_course_page()
+        logging.info("Completed scrape")
 
     def _wait_for_element_by(self, by: By, name: str, timeout=30) -> None:
         try:
             element_present = EC.presence_of_element_located((by, name))
             WebDriverWait(self.driver, timeout).until(element_present)
         except TimeoutException:
-            logging.warning(f"Timeout occurred after {timeout} seconds")
+            logging.warning(f"Timeout occurred after {timeout} seconds. Quitting...")
             self.driver.quit()
+    
+    def _search_in_page(self, text) -> bool:
+        return self.driver.execute_script(f'return document.body.innerHTML.includes("{text}")')
+
+    def _refresh_course_page(self) -> None:
+        self.driver.get(HOME_URL)
+        self._wait_for_element_by(By.XPATH, COURSE_BTN)
+        self.driver.get(SIGN_UP_URL)
 
 
