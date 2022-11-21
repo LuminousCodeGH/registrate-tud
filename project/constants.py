19810a00ef5325c4887f13a30cd1f3235fb6a8bc
LuminousCodeGH <jacob@reaves.org>
2022-11-22 00:22:00 +0100

    Moved urls and xpaths to constants.py

---
+++ project/constants.py
@@ -0,0 +1,9 @@
+# Ignore this part, it's a mess and nothing can be done about it...
+COURSE_BOX = '/html/body/ion-app/app-root/ion-app-wrapper/ion-split-pane/ion-nav/page-enroll-course/enroll-base-component/osi-page-left/ion-content/div[2]/div/ \
+              osi-enroll-course-flow/div/div/osi-course-enroll-search/osi-elastic-search/ion-row/ion-col[2]/div/div/div[2]/div[2]/ion-list/div'
+COURSE_BTN = '/html/body/ion-app/app-root/ion-app-wrapper/ion-split-pane/ion-nav/page-home/ion-content/div[2]/div/ion-grid/ion-row[2]/ion-col/osi-home-shortcuts/ \
+              div/ion-row/ion-col/div[4]/osi-home-shortcut-item/div/ion-row[1]/ion-col/osi-rounded-icon-button/button'
+
+# URLs
+SIGN_UP_URL = "https://my.tudelft.nl/#/inschrijven/cursus/:id"
+HOME_URL = "https://my.tudelft.nl/#/home"
