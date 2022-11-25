6cf5dd6607b1829b58cd7c50107a2562bfd2aa8d
LuminousCodeGH <jacob@reaves.org>
2022-11-25 20:56:00 +0100

    Added/fixed scraper registering available courses

--- project/__main__.py
+++ project/__main__.py
@@ -1,9 +1,11 @@
-from utils import create_webdriver
 from scraper import Scraper
 from courses import Courses
 from data.prefs import browser
 import logging
 import argparse
+import utils
+import getpass
+
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s - %(name)s - %(message)s', level=logging.INFO)
@@ -11,14 +13,38 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(prog="RegistrateTUD", description="Get informed about upcoming exams!")
     argparser.add_argument("-a", "--add_courses", action="store_const", default=False,
                            help="Add courses to CSV file (default=False)", const=True)
+    argparser.add_argument("-i", "--initial_setup", action="store_const", default=False,
+                           help="Runs the program as if it were the first time", const=True)
     args: dict[str] = vars(argparser.parse_args())
 
+    if (browser != "firefox" and browser != "chrome"):
+        raise ValueError("Browser must be firefox or chrome!")
+
     courses = Courses.create_courses_from_path("./courses.csv")
-    if (args["add_courses"]):
+    if (args["initial_setup"]):
+        creds: dict[str] = {}
+        logging.info("Running initial setup...")
+        creds["net_id"]: str = input("Please input your TU Delft net id: ")
+        net_pass: str = getpass.getpass("Please input the corresponding password (hidden): ")
+        print("The sender email is logged into to send emails in case there are open sign ups")
+        print("It is recommended you use an unimportant or dedicated script email")
+        creds["sender_mail"]: str = input("Please input the sender email: ")
+        sender_pass: str = getpass.getpass("Please input the corresponding password: ")
+        print("The receiver email is the email the sender sends the notifications to")
+        print("This can be the same as the sender email")
+        creds["receiver_mail"] = input("Please input the receiver email: ")
+        creds["net_pass"] = utils.encode_string(net_pass)
+        creds["mail_pass"] = utils.encode_string(sender_pass)
+        utils.save_to_json(creds)
+    if (args["add_courses"] or args["initial_setup"]):
+        logging.info("Adding courses...")
         courses.add_courses()
         courses.save()
 
-    driver = create_webdriver("firefox")
+    if (not utils.read_from_json()):
+        raise ValueError("Credentials cannot be empty!")
+
+    driver = utils.create_webdriver("firefox")
 
     scraper = Scraper(driver, courses)
     scraper.scrape_for_courses()
