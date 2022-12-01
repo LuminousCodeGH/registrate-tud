6b9876ae9c4f8ccb1ba4bee067b05b1264e4bd2d
LuminousCodeGH <jacob@reaves.org>
2022-12-01 18:22:37 +0100

    Added documentation

--- project/utils.py
+++ project/utils.py
@@ -7,6 +7,20 @@ import os
 
 
 def create_webdriver(browser: str, is_headless=False) -> Chrome | Firefox | None:
+    """
+    Create a webdriver for use in the Scraper class. NOTE: In order for this to work you will have to download
+    a version of the chrome or firefox webdriver. Check the README.md for steps on how to download and set it up.
+
+    Args:
+        browser (str, 'firefox' | 'chrome'): Select which browser the driver should use. NOTE: Chrome gives
+            seemingly random warnings when using that driver, so Firefox is recommended. Though, so far there have not
+            been any performance issues with Chrome despite the warnings.
+        is_headless (bool, optional): Whether or not to hide the browser window when the driver
+            is being run. Defaults to False.
+
+    Returns:
+        Chrome | Firefox | None: The driver which will be used for scraping. In case there is an error, returns None.
+    """
     logging.info(f"Creating {browser} webdriver...")
     if browser != "chrome" and browser != "firefox":
         if browser == "":
@@ -32,6 +46,18 @@ def create_webdriver(browser: str, is_headless=False) -> Chrome | Firefox | None
 
 
 def encode_string(value: str) -> str:
+    """
+    Encodes a string to Base64 and returns it. Used to make passwords unreadable to a user.
+    NOTE: While this adds a minimal layer of protection, this is not as secure as encryption at all, so do not rely
+    on this to keep your passwords secure. It is merely intended to prevent people from looking at your screen and
+    instantly knowing your passwords.
+
+    Args:
+        value (str): The string to encode.
+
+    Returns:
+        str: The Base64 encoded string.
+    """
     value_bytes: bytes = value.encode('ascii')
     value_bytes = base64.b64encode(value_bytes)
     value_b64: str = value_bytes.decode('ascii')
@@ -39,6 +65,15 @@ def encode_string(value: str) -> str:
 
 
 def decode_string(value_b64: str) -> str:
+    """
+    Decodes the Base64 encoded string and returns it.
+
+    Args:
+        value_b64 (str): The Base64 encoded string to decode.
+
+    Returns:
+        str: The decoded string.
+    """
     value_b64_bytes: bytes = value_b64.encode('ascii')
     value_bytes: bytes = base64.b64decode(value_b64_bytes)
     value: str = value_bytes.decode('ascii')
@@ -46,11 +81,27 @@ def decode_string(value_b64: str) -> str:
 
 
 def save_to_json(creds: dict[str], jpath="./data/creds.json") -> None:
+    """
+    Saves credentials to the creds.json.
+
+    Args:
+        creds (dict[str]): A dictionary with the user credentials. Passwords should be encoded before saving.
+        jpath (str, optional): The file path to where to save the credentials. Defaults to "./data/creds.json".
+    """
     with open(jpath, 'w') as f:
         json.dump(creds, f)
 
 
 def read_from_json(jpath="./data/creds.json") -> dict[str]:
+    """
+    Read the user credentials from file or create an empty file in case such file does not exist.
+
+    Args:
+        jpath (str, optional): The file path to where the credentials are saved. Defaults to "./data/creds.json".
+
+    Returns:
+        dict[str]: A dictionary with all of the data read in the saved JSON file.
+    """
     if not os.path.exists(jpath):
         save_to_json({})
         logging.warning("The creds.json file was not found, so an empty one was created!")
